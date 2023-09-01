5084455c7c76b66391bad7fc7353d86e44f461c1
Catherine Lee <csl@fb.com>
2023-09-01 16:49:34 -0700

    update

--- .ci/docker/common.sh
+++ .ci/docker/common.sh
@@ -6,14 +6,14 @@
 # LICENSE file in the root directory of this source tree.
 
 set -ex
-
+PYTHON_VERSION="3.10"
 # shellcheck source=/dev/null
 as_ci_user() {
   # NB: unsetting the environment variables works around a conda bug
   #     https://github.com/conda/conda/issues/6576
   # NB: Pass on PATH and LD_LIBRARY_PATH to sudo invocation
   # NB: This must be run from a directory that the user has access to
-  sudo -E -H -u ci-user env -u SUDO_UID -u SUDO_GID -u SUDO_COMMAND -u SUDO_USER env "PATH=${PATH}" "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" "$@"
+  sudo -E -H -u jenkins env -u SUDO_UID -u SUDO_GID -u SUDO_COMMAND -u SUDO_USER env "PATH=${PATH}" "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" "$@"
 }
 
 pip_install() {
